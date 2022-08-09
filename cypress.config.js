8d3e06b22b8179fe508b23afaf9f1349f52bfe4a
PoPoPo3 <60116442+PoPoPo3@users.noreply.github.com>
2022-08-10 01:58:10 +0300

    Config Work!

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    excludeSpecPattern: "**/2-advanced-examples/*",
+    excludeSpecPattern: "**/1-getting-started/*",
+    baseUrl: "https://rocknrolls23.ru/"
+  }
+})
